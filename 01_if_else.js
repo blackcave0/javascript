38bf543f1a6fe1ffd17b02a207ad2dbda601e95b
blackcave09 <i3intel@protonmail.com>
2020-11-20 22:43:48 +0530

    a

---
+++ 01_if_else.js
@@ -0,0 +1,15 @@
+//:: Fill in the blanks to create a vaild if... else...if Statement :
+
+var status = 1;
+var msg; 
+if (status == 1){
+    msg = "Online";
+    alert(msg);
+}
+else if(status == 2){
+    msg = "Away";
+    alert(msg);
+}else{
+    msg = "Offline";
+    alert(msg);
+}
