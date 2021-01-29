d9b6f2d6e060df9773a14ad78e89f776f4abaff0
blackcave09 <i3intel@protonmail.com>
2021-01-29 20:47:32 +0530

    a

--- test_01.js
+++ test_01.js
@@ -1,8 +1,1 @@
 var user_input = prompt().toLowerCase() || toUppercase();
-
-<<<<<<< HEAD
-// this is Comment
-// this is new Comment
-=======
-// this is Comment
->>>>>>> a80690185d0088e1041f7f7743a8404a92a243f1
